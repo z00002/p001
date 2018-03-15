8f060b43cd313ea6dd7be028b9d9649f14b1c62a
candy gao <z2z00002@outlook.com>
2018-03-15 11:44:45 +0800

    kafka code

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using KafkaNet;
+using KafkaNet.Model;
+using KafkaNet.Protocol;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string payload = "welcome to kafka!";
+            string topic = "IDGTestTopic";
+            Message msg = new Message(payload);
+
+            Uri uri = new Uri("http://192.168.1.17:9092");
+            var options = new KafkaOptions(uri);
+
+            var router = new BrokerRouter(options);
+            var client = new Producer(router);
+
+            client.SendMessageAsync(topic, new List<Message> { msg }).Wait();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using KafkaNet;
+using KafkaNet.Model;
+using KafkaNet.Protocol;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string topic = "IDGTestTopic";
+            Uri uri = new Uri("http://192.168.1.17:9092");
+            var options = new KafkaOptions(uri);
+            var router = new BrokerRouter(options);
+            var consumer = new Consumer(new ConsumerOptions(topic, router));
+            foreach (var message in consumer.Consume())
+            {
+                Console.WriteLine(Encoding.UTF8.GetString(message.Value));
+            }
+            Console.ReadLine();
+        }
+    }
+}
